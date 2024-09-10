2cc2bbfd045ca58319758ff94e1dfbd7dfdfaccf
VIvaan Kishanwala <178980407+vkishanwala7@users.noreply.github.com>
2024-09-10 19:44:37 +0530

    Update 13.c

--- 13.c
+++ 13.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <conio.h>
+//This program will check if a number is odd or even
 int main () 
 {
     long long int a;
     printf("Please enter a integer: "); //For input of integer
     scanf("%d", &a);
-    if(a%2==0) //Calculations for checking odd and even
+    if(a%2==0) //Calculations for checking odd and even and output
     {
         printf("%d is a even number!", a);
     }
